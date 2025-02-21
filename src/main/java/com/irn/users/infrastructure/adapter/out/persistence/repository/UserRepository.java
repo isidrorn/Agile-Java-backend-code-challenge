61073385e75e9b6a1d47e5271a3577a30cd78816
irebollo <isidro.rn@gmail.com>
2025-02-21 23:25:11 +0100

    added method findByName

--- src/main/java/com/irn/users/infrastructure/adapter/out/persistence/repository/UserRepository.java
+++ src/main/java/com/irn/users/infrastructure/adapter/out/persistence/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.irn.users.infrastructure.adapter.out.persistence.repository;
 
-import com.irn.users.infrastructure.adapter.out.persistence.model.User;
+import com.irn.users.infrastructure.adapter.out.persistence.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
 }
